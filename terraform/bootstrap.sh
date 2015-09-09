054d1a71f23753c8d30501ad05ae2b8b53cd88b3
Matteo Ruina <matteo.ruina@gmail.com>
2015-09-09 11:18:38 +0100

    Add bootstrap script

--- terraform/bootstrap.sh
+++ terraform/bootstrap.sh
@@ -2,14 +2,21 @@
 set -e
 
 # Ensure prerequisites are installed
-apt-get update
-apt-get install -y wget git
+sudo apt-get update
+sudo apt-get install -y wget git
 
 # Install salt
 cd /opt
 wget -O install_salt.sh https://bootstrap.saltstack.com
-sh install_salt.sh -P
-service salt-minion stop
+sudo sh install_salt.sh -P
+sudo service salt-minion stop
 
-# Get the data
-git clone 
+# Get the data and copy it to the right destinations
+git clone https://github.com/maruina/acme-esc
+cd acme-esc
+sudo cp -R salt /srv
+sudo cp -R pillar /srv
+sudo mv -f minion /etc/salt/minion
+
+# Let's the magic happens
+sudo salt-call state.highstate
